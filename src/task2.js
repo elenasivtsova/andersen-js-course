148bd7340cb89e315e621b47289b82a8b3181157
esivtsova <e.sivtsova@andersenlab.com>
2019-06-27 08:43:49 +0300

    Ready task3.

--- src/task2.js
+++ src/task2.js
@@ -1,19 +1,22 @@
 export function parseJSON(jsonStr, succCb, failCb) {
-    try {
-      const result = JSON.parse(jsonStr);
+  try {
+    const result = JSON.parse(jsonStr);
     succCb(result);
-    } catch (err) {
-      failCb(err);
-    }
+  } catch (error) {
+    failCb(error);
   }
-  
+}
+
 export function successCb(result) {
+  // eslint-disable-next-line no-console
   console.log(`Success parse!`);
+  // eslint-disable-next-line no-console
   console.log(result);
 }
 
-export function failureCb(err) {
+export function failureCb(error) {
+  // eslint-disable-next-line no-console
   console.log(`Failure parse!`);
-  console.log(err);
+  // eslint-disable-next-line no-console
+  console.log(error);
 }
-
