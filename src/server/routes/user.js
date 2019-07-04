615405e475d19b49534dd4d683bbb3d4507683dd
sivtsova <esivtsovaa@gmail.com>
2019-07-04 21:59:43 +0300

    connect db

--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -2,12 +2,20 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  return res.send(Object.values(req.context.models.users));
+/*userSchema.pre('remove', function (next) {
+  this.model('Message').deleteMany({ user: this._id }, next);
+});*/
+
+router.get('/', async (req, res) => {
+  const users = await req.context.models.User.find();
+  return res.send(users);
 });
 
-router.get('/:userId', (req, res) => {
-  return res.send(req.context.models.users[req.params.userId]);
+router.get('/:userId', async (req, res) => {
+  const user = await req.context.models.User.findById(
+    req.params.userId,
+  );
+  return res.send(user);
 });
 
 export default router;
