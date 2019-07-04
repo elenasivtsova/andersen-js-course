d00cebf3c44dc23e4e837515539cf0672b4674d5
esivtsova <e.sivtsova@andersenlab.com>
2019-07-04 20:39:25 +0300

    routes ready2

---
+++ src/server/models/index.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line prefer-const
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+// eslint-disable-next-line prefer-const
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
+export default {
+  users,
+  messages,
+};
